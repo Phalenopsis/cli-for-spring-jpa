82f8fb8d82516650c2541f2c142b4e71d65df110
Nicolas Leroux <hertford.fr@gmail.com>
2025-01-27 18:24:47 +0100

    feat: add classes for verify if we are in a spring project

---
+++ src/main/java/org/cliforspringjpa/explorer/PomXmlExplorer.java
@@ -0,0 +1,57 @@
+package org.cliforspringjpa.explorer;
+
+import org.cliforspringjpa.exception.SpringProjectException;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Objects;
+
+public class PomXmlExplorer {
+    private static PomXmlExplorer instance;
+    private final String pomXmlPath;
+
+    public static PomXmlExplorer getInstance() {
+        if(Objects.isNull(instance)) {
+            return new PomXmlExplorer();
+        }
+        return instance;
+    }
+
+    private PomXmlExplorer() {
+        pomXmlPath = getPomXmlPath();
+    }
+
+    private String getPomXmlPath() {
+        return  System.getProperty("user.dir") + File.separator  + "pom.xml";
+    }
+
+    public void verifyPomXml() throws SpringProjectException {
+        File pomXmlFile = new File(pomXmlPath);
+        if(!(pomXmlFile.exists() && !pomXmlFile.isDirectory())) {
+            throw new SpringProjectException("pom.xml file does not exists.");
+        }
+    }
+
+    public String getProjectPackage() throws SpringProjectException {
+        String packageName = "";
+        try (
+                BufferedReader reader = new BufferedReader(new FileReader(pomXmlPath))
+        ) {
+            String line;
+            while(Objects.nonNull(line = reader.readLine())) {
+                String OPENING_TAG = "<groupId>";
+                String CLOSING_TAG = "</groupId>";
+                if(line.contains(OPENING_TAG) && line.contains(CLOSING_TAG) ) {
+                    String lineWithoutOpening = line.replace(OPENING_TAG, "");
+                    packageName = lineWithoutOpening.replace(CLOSING_TAG, "").trim();
+                    break;
+                }
+            }
+        } catch (IOException error) {
+            throw new SpringProjectException("Unable to open " + pomXmlPath);
+        }
+        return packageName;
+    }
+}
